6e4942436b63c359660508aa01b809f3133b7ea5
dirkarnez <smalldirkalex@gmail.com>
2026-09-15 08:01:55 +0000

    - wip

--- local-init.sh
+++ local-init.sh
@@ -1,13 +1,30 @@
 # /bin/bash
 
-# Should show 3.8+
-python --version
-
-# Should show 2.25+
+# git version 2.55.0
 git --version
 
-# Should show 3.20+
+# cmake version 3.24.4
 cmake --version
 
+# Python 3.14.7
+python --version
+
+# West version: v1.5.0
 pip install -U west && \
   west --version
+export ARMGCC_DIR="/usr"  # or your installation path
+
+# # # Get the latest SDK from main branch:
+# west init -m https://github.com/nxp-mcuxpresso/mcuxsdk-manifests.git mcuxpresso-sdk && \
+# cd mcuxpresso-sdk && \
+# cd manifests && \
+# git pull && \
+# cd .. && \
+# west update_board --set board evkbmimxrt1170 && \
+cd mcuxpresso-sdk && \
+cd mcuxsdk && \
+west build -b evkbmimxrt1170 examples/demo_apps/hello_world
+#  && \
+# west update && \
+
+# # west update_board --set board aw-xm458 --list-repo && 
