e86716dd8b6c3a8707992d831e21323fae820977
dirkarnez <smalldirkalex@gmail.com>
2026-09-15 09:20:10 +0000

    - working

--- local-init.sh
+++ local-init.sh
@@ -1,5 +1,7 @@
 # /bin/bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
 # git version 2.55.0
 git --version
 
@@ -18,26 +20,18 @@ pip install -U west && \
 cd /opt; \
    wget https://gitlab.arm.com/api/v4/projects/tooling%2Fgnu-toolchains-for-arm/packages/generic/gnu-toolchain/14.2.rel1/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz && \
    tar -xf arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz && \
-   ls
+   cd arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi && \
+   ls -R
+
+cd $SCRIPT_DIR
 
 export ARMGCC_DIR="/opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi"
 
 # # Get the latest SDK from main branch:
-# west init -m https://github.com/nxp-mcuxpresso/mcuxsdk-manifests.git mcuxpresso-sdk && \
+west init -m https://github.com/nxp-mcuxpresso/mcuxsdk-manifests.git mcuxpresso-sdk && \
 cd mcuxpresso-sdk && \
 west update_board --set board evkbmimxrt1170 && \
 cd mcuxsdk && \
-west build -b evkbmimxrt1170 --sysbuild ./examples/multicore_examples/hello_world/primary -Dcore_id=cm7 --config flexspi_nor_debug --toolchain=armgcc -p always -d cmake-build && \
+west build -b evkbmimxrt1170 --sysbuild ./examples/multicore_examples/hello_world/primary -Dcore_id=cm7 --config release --toolchain=armgcc -p always -d cmake-build && \
 echo ok
 
-# # cd manifests && \
-# # git pull && \
-# # cd .. && \
-# # west update_board --set board evkbmimxrt1170 && \
-# cd mcuxpresso-sdk && \
-# cd mcuxsdk && \
-# west build -b evkbmimxrt1170 examples/demo_apps/hello_world
-# #  && \
-# west update && \
-
-# # west update_board --set board aw-xm458 --list-repo && 
