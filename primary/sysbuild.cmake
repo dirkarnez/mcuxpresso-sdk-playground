d236aed309a1e3242a05494d862984b8385619a6
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-17 12:40:39 +0800

    - update primary/sysbuild.cmake

--- primary/sysbuild.cmake
+++ primary/sysbuild.cmake
@@ -5,7 +5,7 @@
 
 ExternalZephyrProject_Add(
         APPLICATION hello_world_secondary_core
-        SOURCE_DIR  ${APP_DIR}/../secondary
+        SOURCE_DIR ./secondary
         board ${SB_CONFIG_secondary_board}
         core_id ${SB_CONFIG_secondary_core_id}
         config ${SB_CONFIG_secondary_config}
