39919b4926a8e6aa9ad0875f7ecc4053ce33ff1d
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-17 17:34:38 +0800

    - update local-init.sh

--- local-init.sh
+++ local-init.sh
@@ -41,7 +41,7 @@ cd mcuxpresso-sdk && \
 west update_board --set board evkbmimxrt1170 && \
 cd $SCRIPT_DIR && \
 source ./mcuxpresso-sdk/mcuxsdk/mcux-env.sh && \
-west build -b evkbmimxrt1170 --sysbuild ./primary -Dcore_id=cm7 --config debug --toolchain=armgcc -p always && \
+west build -b evkbmimxrt1170 --sysbuild ./primary -Dcore_id=cm7 --config debug --toolchain=armgcc -p always -d cmake-build && \
 ls && \
 echo ok
 
