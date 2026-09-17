3c5ab9897d92e7c43b1f6821ff387cd655eca2d6
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-17 17:52:22 +0800

    - delete sysbuild.cmake

--- sysbuild.cmake
+++
@@ -1,9 +0,0 @@
-
-ExternalMCUXProject_Add(
-        APPLICATION hello_world_primary_core
-        SOURCE_DIR  ../primary
-        board ${SB_CONFIG_secondary_board}
-        core_id ${SB_CONFIG_secondary_core_id}
-        config ${SB_CONFIG_secondary_config}
-        toolchain ${SB_CONFIG_secondary_toolchain}
-)
