e53f28df8516325c57fa8411d35f9a24757fafc4
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-17 12:34:42 +0800

    - update sysbuild.cmake

--- sysbuild.cmake
+++ sysbuild.cmake
@@ -1,7 +1,8 @@
+mcuxpresso-sdk-playground/cmake-build/primary
 
 ExternalMCUXProject_Add(
         APPLICATION hello_world_primary_core
-        SOURCE_DIR  ./primary
+        SOURCE_DIR  ../primary
         board ${SB_CONFIG_secondary_board}
         core_id ${SB_CONFIG_secondary_core_id}
         config ${SB_CONFIG_secondary_config}
