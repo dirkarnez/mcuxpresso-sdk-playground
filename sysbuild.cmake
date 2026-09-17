d5f792868d369e9868e4645e6bc681a9a3776778
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-17 11:59:17 +0800

    - add sysbuild.cmake

---
+++ sysbuild.cmake
@@ -0,0 +1,9 @@
+
+ExternalMCUXProject_Add(
+        APPLICATION hello_world_primary_core
+        SOURCE_DIR  ./primary
+        board ${SB_CONFIG_secondary_board}
+        core_id ${SB_CONFIG_secondary_core_id}
+        config ${SB_CONFIG_secondary_config}
+        toolchain ${SB_CONFIG_secondary_toolchain}
+)
