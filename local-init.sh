d03e58dea82775f4948594946f1fc9606ac461c3
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-18 09:29:12 +0800

    - update local-init.sh

--- local-init.sh
+++ local-init.sh
@@ -16,33 +16,40 @@ python --version
 pip install -U west && \
   west --version
 
-# /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc --version
-# arm-none-eabi-gcc (Arm GNU Toolchain 14.2.Rel1 (Build arm-14.52)) 14.2.1 20241119
-cd /opt; \
-   wget https://gitlab.arm.com/api/v4/projects/tooling%2Fgnu-toolchains-for-arm/packages/generic/gnu-toolchain/14.2.rel1/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz && \
-   tar -xf arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz && \
-   cd arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi && \
-   ls -R
-
-cd $SCRIPT_DIR
-
 export ARMGCC_DIR="/opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi"
+export ARMGCC_GCC="$ARMGCC_DIR/bin/arm-none-eabi-gcc"
 export MCUXPRESSO_SDK_DIR="$SCRIPT_DIR/mcuxpresso-sdk/mcuxsdk"
+export ENV_FILE="$MCUXPRESSO_SDK_DIR/mcux-env.sh"
+export BOARD="evkbmimxrt1170"
 
-# # Get the latest SDK from main branch:
-# west init -m https://github.com/nxp-mcuxpresso/mcuxsdk-manifests.git mcuxpresso-sdk && \
-# cd mcuxpresso-sdk && \
-# west update_board --set board evkbmimxrt1170 && \
-# cd mcuxsdk && \
-# west build -b evkbmimxrt1170 --sysbuild ./examples/multicore_examples/hello_world/primary -Dcore_id=cm7 --config release --toolchain=armgcc -p always -d cmake-build && \
-# echo ok
 
-west init -m https://github.com/nxp-mcuxpresso/mcuxsdk-manifests.git mcuxpresso-sdk && \
-cd mcuxpresso-sdk && \
-west update_board --set board evkbmimxrt1170 && \
+if [ ! -f "$ARMGCC_GCC" ]; then
+    echo "$ARMGCC_GCC not found. Downloading now..."
+
+    # /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc --version
+    # arm-none-eabi-gcc (Arm GNU Toolchain 14.2.Rel1 (Build arm-14.52)) 14.2.1 20241119
+    cd /opt; \
+      wget https://gitlab.arm.com/api/v4/projects/tooling%2Fgnu-toolchains-for-arm/packages/generic/gnu-toolchain/14.2.rel1/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz && \
+      tar -xf arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz && \
+      cd arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi && \
+      ls -R
+  
+else
+    echo "$ARMGCC_GCC already exists. Skipping download."
+fi
+
+
+if [ ! -f "$ENV_FILE" ]; then
+    echo "$ENV_FILE not found. Downloading now..."
+    west init -m https://github.com/nxp-mcuxpresso/mcuxsdk-manifests.git mcuxpresso-sdk && \
+    cd mcuxpresso-sdk && \
+    west update_board --set board $BOARD && \
+    echo "SDK is ok"
+else
+    echo "$ENV_FILE already exists. Skipping download."
+fi
+
 cd $SCRIPT_DIR && \
-source ./mcuxpresso-sdk/mcuxsdk/mcux-env.sh && \
-west build -b evkbmimxrt1170 --sysbuild ./primary -Dcore_id=cm7 --config debug --toolchain=armgcc -p always -d cmake-build && \
-ls && \
+source $ENV_FILE && \
+west build -b $BOARD --sysbuild ./primary -Dcore_id=cm7 --config debug --toolchain=armgcc -p always -d cmake-build && \
 echo ok
-
