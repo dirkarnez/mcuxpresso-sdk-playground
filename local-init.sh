3f59eff439809381314e8e7a36ed38e5b8383b43
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-24 11:25:38 +0800

    - update local-init.sh

--- local-init.sh
+++ local-init.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 export MCUXPRESSO_SDK_DIR="$SCRIPT_DIR/mcuxpresso-sdk/mcuxsdk"
 export ENV_FILE="$MCUXPRESSO_SDK_DIR/mcux-env.sh"
+export BOARD="evkbmimxrt1170"
 
 case "$(uname -s)" in
 Linux*)
